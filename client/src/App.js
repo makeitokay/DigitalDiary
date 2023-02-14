a8ad8ec49b68b4397929c5fd0a254a1586be7aab
makeitokay <andrey.mu11@yandex.ru>
2023-02-14 20:41:45 +0300

    install react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ function App() {
           <div className="row min-vh-100">
             <div className="col d-flex flex-column justify-content-center align-items-center">
               <img src={logo} className="App-logo" alt="logo" />
-              <h1>Digital Diary</h1>
             </div>
           </div>
         </div>
