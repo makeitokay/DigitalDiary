b43f67d7871de9529513b668e16a044f81898b02
makeitokay <andrey.mu11@yandex.ru>
2023-03-05 02:17:16 +0300

    useless

--- Infrastructure/Repositories/Repository.cs
+++ Infrastructure/Repositories/Repository.cs
@@ -9,7 +9,6 @@ public interface IRepository<TEntity> where TEntity : BaseEntity
 	Task<TEntity> UpdateAsync(TEntity entity);
 	Task DeleteAsync(TEntity entity);
 	Task<TEntity> GetAsync(int id);
-	Task DeleteRangeAsync(IEnumerable<TEntity> entities);
 	IQueryable<TEntity> Items { get; }
 }
 
@@ -44,12 +43,6 @@ public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEnti
 	public async Task DeleteAsync(TEntity entity)
 	{
 		Set.Remove(entity);
-		await DbContext.SaveChangesAsync();
-	}
-
-	public async Task DeleteRangeAsync(IEnumerable<TEntity> entities)
-	{
-		Set.RemoveRange(entities);
 		await DbContext.SaveChangesAsync();
 	}
 
